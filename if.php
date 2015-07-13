b0d18bcfcb48247361ac9456ee31394fc3503bd6
Christian Tobias <cwtobias6@gmail.com>
2015-07-13 10:28:14 -0500

    "updated if/else"

--- if.php
+++ if.php
@@ -4,47 +4,74 @@
  $b = 10;
  $c = '10';
 
- if ($a < $b) {
-     // output the appropriate result
-     echo "$a is less than $b\n";
- }
+ // if ($a < $b) {
+ //     // output the appropriate result
+ //     echo "$a is less than $b\n";
+ // }
 
- if ($b > $a) {
-     // output the appropriate result
-     echo "$b is greater than $a\n";
+ // if ($b > $a) {
+ //     // output the appropriate result
+ //     echo "$b is greater than $a\n";
+ // }
+
+ // if ($b >= $c) {
+ //     // output the appropriate result
+ //     echo "$b is greater than of equal to $c\n";
+ // }
+
+ // if ($b <= $c) {
+ //     // output the appropriate result
+ //     echo "$b is less than or equal to $c\n";
+ // }
+
+ // if ($b == $c) {
+ //     // output the appropriate result
+ //     echo "$b is equal to $c\n";
+ // }
+
+ // if ($b === $c) {
+ //     // output the appropriate result
+ //     echo "$b is equal to $c\n";
+ // }
+
+ // // output the appropriate result
+ // if ($b != $c) {
+ //     echo "$b is not equal to $c\n";
+ // }
+
+ // // output the appropriate result
+ // if ($b !== $c) {
+ //     echo "$b is not identical to $c\n";
+ // }
+
+
+
+ if ($a < $b) {
+     echo "{$a} is less than {$b} \n";
+ } else if ($a > $b) {
+     echo "{$b} is greater than {$a} \n";
  }
 
  if ($b >= $c) {
-     // output the appropriate result
-     echo "$b is greater than of equal to $c\n";
+     echo "{$b} is greater than or equal to {$c}\n";
+ } else if ($b <= $c) {
+     echo "{$b} is less than or eqaul to {$c}\n";
  }
 
- if ($b <= $c) {
-     // output the appropriate result
-     echo "$b is less than or equal to $c\n";
- }
-
- if ($b == $c) {
-     // output the appropriate result
-     echo "$b is equal to $c\n";
- }
 
  if ($b === $c) {
-     // output the appropriate result
-     echo "$b is equal to $c\n";
+     echo "{$b} is identical to {$c} \n";
+ } else if ($b == $c) {
+     echo "{$b} is equal to {$c} \n";
+ } else if ($b !== $c) {
+     echo "{$b} is not identical to {$c} \n";
+ } else if ($b != $c) {
+     echo "{$b} is not equal to {$c} \n";
+ } else {
+     echo "Hey mama \n";
  }
 
- // output the appropriate result
- if ($b != $c) {
-     echo "$b is not equal to $c\n";
- }
-
- // output the appropriate result
- if ($b !== $c) {
-     echo "$b is not identical to $c\n";
- }
-
-
+ 
 
 
 ?>
