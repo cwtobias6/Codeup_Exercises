affc3e7368862cf852879eeb19d79b5e0bd0ebab
Christian Tobias <cwtobias6@gmail.com>
2015-07-15 13:30:44 -0500

    step 3 of functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,84 @@
 <?php
 
+
+
 function add($a, $b)
 {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+		if($b == 0){
+    		return "error, can't divide by zero";
+    	} else {
+    		return $a + $b;
+    	}
+    } else {
+    	return "ERROR: $a and $b must both be numbers\n";
+
+    }
 }
 
 function subtract($a, $b)
 {
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+		if($b == 0){
+    		return "error, can't divide by zero";
+    	} else {
+    		return $a - $b;
+    	}
+	} else {
+		return "ERROR: $a and $b must both be numbers\n";
+	}
 }
 
 function multiply($a, $b)
 {
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+    	if($b == 0){
+    		return "error, can't divide by zero";
+    	} else {
+    		return $a * $b;
+    	}
+    } else {
+    	return "ERROR: $a and $b must both be numbers\n";
+
+    }
 }
 
 function divide($a, $b)
 {
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+    	if($b == 0){
+    		return "error, can't divide by zero";
+    	} else {
+    		return $a / $b;
+    	}
+    } else {
+    	return "ERROR: $a and $b must both be numbers\n";
+
+    }
 }
 
-// Add code to test your functions here
+function modulous($a,$b)
+{
+	if (is_numeric($a) && is_numeric($b)) {
+		if($b == 0){
+    		return "error, can't divide by zero";
+    	} else {
+    		return $a % $b;
+    	}
+	} else {
+		return "ERROR: $a and $b must both be numbers\n";
+	}
+
+}
+
+function error()
+
+echo add(5,10);
+
+echo subtract(5,10);
+
+echo multiply(5,10);
+
+echo divide(5,10);
+
+echo modulous(5,10);
