635b890d831f710ed55b8dd0c71727d84358ee80
Christian Tobias <cwtobias6@gmail.com>
2015-07-22 20:08:36 -0500

    exercise

--- contact-parser.php
+++ contact-parser.php
@@ -1,1 +1,20 @@
+<?php
 
+function parseContacts($filename)
+{
+    $contacts = [];
+
+    $filename = 'contacts.txt';
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    $contentsArray = explode("\n", $contents);
+
+    foreach ($contentsArray as $key => $value) {
+    	explode("|", $contentsArray);
+    }
+    print_r($contentsArray);
+    return $contacts;
+    fclose($handle);
+}
+
+var_dump(parseContacts('contacts.txt'));
