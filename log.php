5dc9c403b7025fd5bab6bc656292fc7c0a5c555b
Christian Tobias <cwtobias6@gmail.com>
2015-07-24 11:56:48 -0500

    "updated log classes with constructor and destructor"

--- log.php
+++ log.php
@@ -4,25 +4,35 @@ class Log
 {
 
 	public $filename;
+	public $handle;
 
+
+	public function __construct($prefix = 'log') {
+		$newDay = date("Y-m-d");
+		$this->filename = $prefix . "-" . $newDay . ".log";
+		$this->handle = fopen($this->filename, 'a');
+	}
+
+	
 	public function logMessage($logLevel, $message)
 	{
 		$newLog = date('Y-m-d H:i:s');
-		$handle = fopen($this->filename, 'a');
 		
-		fwrite($handle, PHP_EOL . $newLog . " " . $logLevel . " " . $message);
+		fwrite($this->handle, PHP_EOL . $newLog . " " . $logLevel . " " . $message);
 
-		fclose($handle);
 	}
 
-	function info($message){
+	public function info($message){
 		$this->logMessage("INFO", $message);
 	}
 
-	function error($message){
+	public function error($message){
 		$this->logMessage("ERROR", $message);
 	}
 
+	public function __destruct(){
+		fclose($this->handle);
+	}
 
 }
 
--- log_test.php
+++ log_test.php
@@ -1,15 +1,19 @@
 <?php
 require_once 'Log.php';
 
-$firstLog = new Log();
-$newDay = date("Y-m-d");
-$firstLog->filename = "log-" . $newDay . ".log";
+// $firstLog = new Log();
+// $newDay = date("Y-m-d");
+// $firstLog->filename = "log-" . $newDay . ".log";
 
 
+// echo $firstLog->logMessage("loggy lvl done for the day", "messaging");
+// echo $firstLog->info("this is info message");
+// echo $firstLog->error("this is error message");
 
-echo $firstLog->logMessage("loggy lvl done for the day", "messaging");
-echo $firstLog->info("this is info message");
-echo $firstLog->error("this is error message");
+
+$loggy = new Log('cli');
+echo $loggy->info("this is an info message");
+
 
 
 ?>
