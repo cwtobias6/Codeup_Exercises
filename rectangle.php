cb423ffbc466431ef23ebcd89d0a507cc159d71e
Christian Tobias <cwtobias6@gmail.com>
2015-08-05 14:30:56 -0500

    updated private and protected

--- rectangle.php
+++ rectangle.php
@@ -3,24 +3,42 @@
 class Rectangle
 {
 
-	public $height;
-	public $width;
+	private $height;
+	private $width;
 
 	public function __construct($height,$width)
 	{
 
-		$this->height = $height;
-        $this->width  = $width;
+		$this->setHeight($height);
+        $this->setWidth($width);
 
 
 	}
 
+	protected function setHeight($height) {
+		$this->height = $height;
+
+	}
+	protected function setWidth($width) {
+		$this->width = $width;
+	}
+	public function getHeight() {
+		return $this->height;		
+	}
+	public function getWidth() {
+		return $this->width;
+	}
 
 	public function area() 
     {
 
-    	return $this->height * $this->width;
+    	return $this->getHeight() * $this->getWidth();
 
+
+    }
+    public function perimeter() 
+    {
+    	return (2 * $this->getHeight()) + (2 * $this->getWidth());
 
     }
 
