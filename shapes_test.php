866a06eb6810bfe6e733b5a9c1d60de32b6ea784
Christian Tobias <cwtobias6@gmail.com>
2015-08-05 14:31:07 -0500

    updated private and protected

--- shapes_test.php
+++ shapes_test.php
@@ -7,7 +7,7 @@ require_once 'square.php';
 $rectangle = new Rectangle(5,6);
 echo $rectangle->area() . PHP_EOL;
 
-$square = new Square(4,4);
+$square = new Square(4);
 echo $square->area() . PHP_EOL;
 echo $square->perimeter() . PHP_EOL;
 
